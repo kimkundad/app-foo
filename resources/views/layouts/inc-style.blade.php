a8def307db56c7502d5322a5170b6ce8303aa38a
kimkundad <kim.kundad@gmail.com>
2023-06-11 18:35:57 +0700

    kim

--- resources/views/layouts/inc-style.blade.php
+++ resources/views/layouts/inc-style.blade.php
@@ -3,6 +3,6 @@
         <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
         <link rel="stylesheet" href="{{ url('/home/assets/css/bootstrap.css') }}" type="text/css" />
         <link rel="stylesheet" href="{{ url('/home/assets/css/theme.css') }}" type="text/css" />
-        <link rel="stylesheet" href="{{ url('/home/assets/css/style.css') }}" type="text/css" />
+        <link rel="stylesheet" href="{{ url('/home/assets/css/style.css') }}?v{{time()}}" type="text/css" />
         <link rel="stylesheet" href="{{ url('/home/libs/font-awesome/css/font-awesome.css') }}" type="text/css" />
 <!-- endbuild -->
